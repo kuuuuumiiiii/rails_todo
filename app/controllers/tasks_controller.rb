c1fcbc58c6f7e550c29d412ddc1efcf7b7f543bc
Ubuntu <ubuntu@ip-172-31-21-142.ap-northeast-1.compute.internal>
2023-07-23 03:38:15 +0000

    todoリスト作成

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,58 @@
+class TasksController < ApplicationController
+  before_action :set_task, only: [:edit, :update, :destroy, :toggle]
+  def index
+    @tasks = Task.all
+  end
+  
+  def new
+    @task = Task.new
+  end
+  
+  def create
+    @task = Task.new(task_params)
+    if @task.save
+      redirect_to root_path
+    else
+      render 'new', status: :unprocessable_entity
+    end
+  end
+  
+  def edit
+    # p params[:id]
+    # @task = Task.find(params[:id])
+  end
+  
+  def update
+    # @task = Task.find(params[:id])
+    if @task.update(task_params)
+      redirect_to root_path
+    else
+      render 'edit', status: :unprocessable_entity
+    end
+  end
+  
+  def destroy
+    # @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to root_path, status: :see_other
+  end
+  
+  def toggle
+    # @task = Task.find(params[:id])
+    @task.update(completed: !@task.completed)
+    render turbo_stream: turbo_stream.replace(
+      @task,
+      partial: 'completed',
+      locals: { task: @task}
+      )
+  end
+  
+  private
+  def task_params
+    params.require(:task).permit(:title)
+  end
+  
+  def set_task
+    @task = Task.find(params[:id])
+  end
+end
